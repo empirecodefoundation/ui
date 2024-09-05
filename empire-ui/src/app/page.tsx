6eb82656cc202fb839e772e0d3758f71c6124c27
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-09-05 18:38:55 +0530

    Add prediction-output-card

--- empire-ui/src/app/page.tsx
+++ empire-ui/src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+import { PredictionOutputCardDemo1 } from "@/components/templates/prediction-output-card-demo1";
 
-import { Card1Demo } from "@/components/templates/Card1Demo";
-
-export default function Component() {
+export default function Page() {
   return (
-    <div className="p-6 h-screen">
-      <div className="flex gap-5 flex-auto flex-wrap">
-        <Card1Demo />
-      </div>
-    </div>
+    <>
+      <PredictionOutputCardDemo1 />
+    </>
   );
 }
