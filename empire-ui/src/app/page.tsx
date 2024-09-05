43465c9c45794b048094142dd2b4bd0e63e9259e
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-09-06 02:07:00 +0530

    Fix issue #11: Add dataset overview card

--- empire-ui/src/app/page.tsx
+++ empire-ui/src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+import { DatasetOverviewCardDemo1 } from "@/components/templates/Dataset-Overview-Card-Demo1";
 
-import { Card1Demo } from "@/components/templates/Card1Demo";
-
-export default function Component() {
+export default function Page() {
   return (
-    <div className="p-6 h-screen">
-      <div className="flex gap-5 flex-auto flex-wrap">
-        <Card1Demo />
-      </div>
-    </div>
+    <>
+      <DatasetOverviewCardDemo1 />
+    </>
   );
 }
