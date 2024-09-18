14c39edc5c4280ec529956e6c4914903afde8aa9
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-09-19 01:08:36 +0530

    fix/minor ui fixes

--- empire-ui/src/app/(home)/page.tsx
+++ empire-ui/src/app/(home)/page.tsx
@@ -13,48 +13,21 @@ import Footer from "@/components/common/Footer";
 import LandingCard2 from "@/components/common/landing-card2";
 import JapaneseCharacter from "@/components/common/japanese";
 
-// Round number up to nearest step for better canvas performance
-const round = (n: number, step = 20) => Math.ceil(n / step) * step;
-
-// Try making me lower to see how performance degrades
-const STEP = 30;
-
 const particleOptions: ParticleOptions = {
   filter: ({ x, y, image }) => {
     // Get pixel
     const pixel = image.get(x, y);
-    // Make a particle for this pixel if magnitude < 200 (range 0-255)
-    const magnitude = (pixel.r + pixel.g + pixel.b) / 3;
-    return magnitude < 200;
+    // Make a particle for this pixel if blue > 50 (range 0-255)
+    return pixel.b > 50;
   },
-  color: ({ x, y, image }) => {
-    const pixel = image.get(x, y);
-    // Canvases are much more performant when painting as few colors as possible.
-    // Use color of pixel as color for particle however round to nearest 30
-    // to decrease the number of unique colors painted on the canvas.
-    // You'll notice if we remove this rounding, the framerate will slow down a lot.
-    return `rgba(
-      ${round(pixel.r, STEP)}, 
-      ${round(pixel.g, STEP)}, 
-      ${round(pixel.b, STEP)}, 
-      ${round(pixel.a, STEP) / 255}
-    )`;
-  },
-  radius: ({ x, y, image }) => {
-    const pixel = image.get(x, y);
-    const magnitude = (pixel.r + pixel.g + pixel.b) / 3;
-    // Lighter colors will have smaller radius
-    return 3 - (magnitude / 255) * 1.5;
-  },
-  mass: () => 40,
-  friction: () => 0.15,
-  initialPosition: ({ canvasDimensions }) => {
-    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
-  },
+  color: ({ x, y, image }) => "#ddd",
+  radius: () => Math.random() * 1.5 + 0.5,
+  mass: () => 70,
+  friction: () => 0.25,
 };
 
 const motionForce = (x: number, y: number): ParticleForce => {
-  return forces.disturbance(x, y, 5);
+  return forces.disturbance(x, y, 8);
 };
 
 const page = () => {
@@ -69,7 +42,7 @@ const page = () => {
             entropy={2}
             width={1500}
             height={460}
-            maxParticles={8000}
+            maxParticles={20000}
             particleOptions={particleOptions}
             mouseMoveForce={motionForce}
             touchMoveForce={motionForce}
