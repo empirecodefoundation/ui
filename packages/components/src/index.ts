1f313fca84b4f818105217ad0c99afaea16261b4
shashwatS22 <shashwat.dehradun@gmail.com>
2025-03-24 11:49:07 +0530

    multiple files in a component supported

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,1 +1,3 @@
+// @ts-nocheck
 export * from "./components/ui/button";
+export * from "./components/ui/chatbot";
--- packages/components/src/lib/utils.ts
+++ packages/components/src/lib/utils.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
